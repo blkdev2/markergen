c49e6d1b07b18c2c5105e22421b9de91687787eb
Brian Kauke <kaukeb@gmail.com>
2014-05-05 19:35:26 -0700

    Renamed to plusmark

--- markersheet.py
+++ markersheet.py
@@ -1,6 +1,6 @@
 from fpdf import FPDF
 import simple
-import pdftag
+import pdfmarker
 
 def thirty_simple_marker_sheet(start_id):
     pdf = FPDF(unit="mm", format="letter")
@@ -9,14 +9,13 @@ def thirty_simple_marker_sheet(start_id):
         x_sp, y_sp = start_position
         for i in range(5):
             pattern = simple.SimplePattern(start_id + i)
-            pdftag.draw_marker(pdf, pattern,
-                               (x_sp + i * 40, y_sp),
-                               (30, 30),
-                               5)
+            pdfmarker.draw_marker(pdf, pattern,
+                                  (x_sp + i * 40, y_sp),
+                                  (30, 30),
+                                  5)
     for i in range(6):
         draw_row(start_id + i * 5, (12, 20 + 40 * i))
     return pdf
-
 
 if __name__ == "__main__":
     pdf = thirty_simple_marker_sheet(0)
---
+++ readme.md
@@ -0,0 +1,14 @@
+plusmark
+========
+
+Python code to create ARToolkitPlus markers.
+
+markersheet.py
+--------------
+
+Create a sheet of 30 'simple' markers. Requires pyfpdf.
+
+rimarker.py
+-----------
+
+Renders a marker to a RenderMan stream. Requires cgkit and a RenderMan compatible renderer.
